b13bcda63b9aa4612495576772141dda594cfe75
delya <128806178+avrich@users.noreply.github.com>
2023-10-30 14:57:50 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace triangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(174, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(477, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите стороны треугольника";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(74, 217);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(149, 22);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(337, 217);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(154, 22);
+            this.textBoxB.TabIndex = 2;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(599, 217);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(145, 22);
+            this.textBoxC.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace triangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool flagA = double.TryParse(textBoxA.Text, out double a);
+            if (!flagA)
+            {
+                MessageBox.Show("Вы не ввели значение стороны A либо ввели не число");
+                textBoxA.Text = string.Empty;
+                textBoxB.Text = string.Empty;
+                textBoxC.Text = string.Empty;
+                return;
+
+            }
+            bool flagB = double.TryParse(textBoxB.Text, out double b);
+            if (!flagB)
+            {
+                MessageBox.Show("Вы не ввели значение стороны B либо ввели не число");
+                textBoxA.Text = string.Empty;
+                textBoxB.Text = string.Empty;
+                textBoxC.Text = string.Empty;
+                return;
+
+            }
+            bool flagC = double.TryParse(textBoxC.Text, out double c);
+            if (!flagC)
+            {
+                MessageBox.Show("Вы не ввели значение стороны C либо ввели не число");
+                textBoxA.Text = string.Empty;
+                textBoxB.Text = string.Empty;
+                textBoxC.Text = string.Empty;
+                return;
+
+            }
+            if (a <= 0 || c <= 0 || b <= 0)
+            {
+                MessageBox.Show("Вы указали сторону, которая меньше либо равна 0!");
+                textBoxA.Text = string.Empty;
+                textBoxB.Text = string.Empty;
+                textBoxC.Text = string.Empty;
+                return;
+            }
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                MessageBox.Show("Одна сторона больше суммы двух других или равна ей!");
+                textBoxA.Text = string.Empty;
+                textBoxB.Text = string.Empty;
+                textBoxC.Text = string.Empty;
+                return;
+            }
+            if (a > 1000000 || b > 1000000 || c > 1000000)
+            {
+                MessageBox.Show("Вы ввелиre слишком большое число!");
+                textBoxA.Text = string.Empty;
+                textBoxB.Text = string.Empty;
+                textBoxC.Text = string.Empty;
+                return;
+            }
+            if (a == b & b == c)
+            {
+                MessageBox.Show("Треугольник является равносторонним");
+                return;
+
+            }
+            if (a == b || b == c || a == c)
+            {
+                MessageBox.Show("Треугольник является равнобедренным");
+                return;
+
+            }
+            if (a != b & b != c)
+            {
+                MessageBox.Show("Треугольник является разносторонним");
+                return;
+            }
+        }
+    }
+}
